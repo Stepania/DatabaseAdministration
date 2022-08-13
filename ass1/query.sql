92a95efec9231dc9e9aa0448293c7c162afe8817
Stepania <super.kydryavuy2011@yandex.ru>
2022-08-13 14:23:31 +1200

    Iteration1AlmostDone

--- ass1/query.sql
+++ ass1/query.sql
@@ -1,23 +1,30 @@
 use PalliativeCare;
 
-
--- 1.Display all patients who older than 70 years and live in south Island
+-- 1.Display all patients who older than 70 years and live in South Island, it means DOB myst be minimum on 1952
 select concat(firstName, ' ',lastName) as 'Patient full name',TIMESTAMPDIFF(YEAR, DOB, CURDATE()) AS 'age'
 from patient
 where district = 'South Island'
 and
 TIMESTAMPDIFF(YEAR, DOB, CURDATE())>70;
 
--- 2. Display all carers who have more than 1 patient*/
-select * from patientCarers;
+-- checking or testing
+select *from patient
+where DOB < '1952-05-30'
+and district = "South Island";
+-- answer is 5 people 
 
-select concat(c.firstName, ' ', c.lastName) as 'Carer fullname',count(c.carerId) as amountPatients, concat(p.firstName, ' ', p.lastName) as 'Patient full name',pc.relationshipToPatient
+-- 2. Display all carers who have more than 1 patient*/
+select * from patientcarers;
+
+select concat(c.firstName, ' ', c.lastName) as 'Carer fullname',count(c.carerId) as amountPatients,
+concat(p.firstName, ' ', p.lastName) as 'Patient full name',pc.relationshipToPatient
 from patient p, carer c, patientCarers pc
 where pc.patientId = p.patientId and pc.carerId = c.carerId
 group by p.patientId
 having  count(pc.patientId) > 1;
 
-
+-- checking/testing only caredId=14 have more than one patient
+select * from patientcarers;
 
 -- 3. How many times carer "Marcia Chaney" made a contact aka carerId-14 
 select count(con.carerId) as amountCalls, concat(c.firstName, ' ', c.lastName) as 'Carer fullname'
@@ -28,14 +35,3 @@ where firstName = "Marcia"  and lastName = "Chaney";
 select * from Contactor
 where carerId=14;
 -- right answer is 2!!
-
-
-
-
-
-
-
-SELECT COUNT(b.[room_type_requested]) as 'Total_Family_Rooms'
-FROM [dbo].[guest] g
-INNER JOIN [dbo].[booking] b ON g.[id] = b.[guest_id]
-WHERE b.[room_type_requested] = 'family'
